11bcafe93580cd1018448666521fbfcf2e288f4e
V <V@DESKTOP-2KS75VT>
2024-07-24 02:49:47 +0100

    Fast modulo rewritten using string concatination and StringBuilder

--- ELO/ELO/PointOperations/MathUtilities.cs
+++ ELO/ELO/PointOperations/MathUtilities.cs
@@ -5,7 +5,7 @@ namespace ELO.PointOperations;
 public static class MathUtilities
 {
     private static readonly BigInteger P256 = Curve.P;
-    private static readonly BigInteger P256X2 = P256 * 2;
+    private static readonly BigInteger P256X2 = P256 << 1;
     private static readonly int[] Index = [0, 32, 64, 96, 128, 160, 192, 224];
 
     private static readonly int twoToW = 1 << Curve.WindowSize;
@@ -82,83 +82,181 @@ public static class MathUtilities
 
     public static BigInteger FastModuloP256(BigInteger a)
     {
-        uint[] aParts = new uint[16];
-        for (int i = 0; i < 16; i++)
+        var aParts = new uint[16];
+        for (var i = 0; i < 16; i++)
         {
             aParts[i] = (uint)((a >> (i * 32)) & 0xFFFFFFFF);
         }
 
+        var hexParts = new string[16];
+        for (var i = 0; i < 16; i++)
+        {
+            hexParts[i] = aParts[i].ToString("X8");
+        }
 
-        BigInteger t = ((BigInteger)aParts[7] << Index[7]) +
-                       ((BigInteger)aParts[6] << Index[6]) +
-                       ((BigInteger)aParts[5] << Index[5]) +
-                       ((BigInteger)aParts[4] << Index[4]) +
-                       ((BigInteger)aParts[3] << Index[3]) +
-                       ((BigInteger)aParts[2] << Index[2]) +
-                       ((BigInteger)aParts[1] << Index[1]) +
-                                    aParts[0];
+        var zeros = "00000000";
 
-        BigInteger s1 = ((BigInteger)aParts[15] << Index[7]) +
-                        ((BigInteger)aParts[14] << Index[6]) +
-                        ((BigInteger)aParts[13] << Index[5]) +
-                        ((BigInteger)aParts[12] << Index[4]) +
-                        ((BigInteger)aParts[11] << Index[3]);
+        var tString = "0" + hexParts[7] + hexParts[6] + hexParts[5] + hexParts[4] + hexParts[3] + hexParts[2] + hexParts[1] + hexParts[0];
+        var s1String = hexParts[15] + hexParts[14] + hexParts[13] + hexParts[12] + hexParts[11] + zeros + zeros + zeros;
+        var s2String = zeros + hexParts[15] + hexParts[14] + hexParts[13] + hexParts[12] + zeros + zeros + zeros;
+        var s3String = hexParts[15] + hexParts[14] + zeros + zeros + zeros + hexParts[10] + hexParts[9] + hexParts[8];
+        var s4String = hexParts[8] + hexParts[13] + hexParts[15] + hexParts[14] + hexParts[13] + hexParts[11] + hexParts[10] + hexParts[9];
+        var d1String = hexParts[10] + hexParts[8] + zeros + zeros + zeros + hexParts[13] + hexParts[12] + hexParts[11];
+        var d2String = "0" + hexParts[11] + hexParts[9] + zeros + zeros + hexParts[15] + hexParts[14] + hexParts[13] + hexParts[12];
+        var d3String = hexParts[12] + zeros + hexParts[10] + hexParts[9] + hexParts[8] + hexParts[15] + hexParts[14] + hexParts[13];
+        var d4String = "0" + hexParts[13] + zeros + hexParts[11] + hexParts[10] + hexParts[9] + zeros + hexParts[15] + hexParts[14];
 
-
-        BigInteger s2 = ((BigInteger)aParts[15] << Index[6]) +
-                        ((BigInteger)aParts[14] << Index[5]) +
-                        ((BigInteger)aParts[13] << Index[4]) +
-                        ((BigInteger)aParts[12] << Index[3]);
-
-        BigInteger s3 = ((BigInteger)aParts[15] << Index[7]) +
-                        ((BigInteger)aParts[14] << Index[6]) +
-                        ((BigInteger)aParts[10] << Index[2]) +
-                        ((BigInteger)aParts[9] << Index[1]) +
-                                     aParts[8];
-
-        BigInteger s4 = ((BigInteger)aParts[8] << Index[7]) +
-                        ((BigInteger)aParts[13] << Index[6]) +
-                        ((BigInteger)aParts[15] << Index[5]) +
-                        ((BigInteger)aParts[14] << Index[4]) +
-                        ((BigInteger)aParts[13] << Index[3]) +
-                        ((BigInteger)aParts[11] << Index[2]) +
-                        ((BigInteger)aParts[10] << Index[1]) +
-                                     aParts[9];
-
-        BigInteger d1 = ((BigInteger)aParts[10] << Index[7]) +
-                        ((BigInteger)aParts[8] << Index[6]) +
-                        ((BigInteger)aParts[13] << Index[2]) +
-                        ((BigInteger)aParts[12] << Index[1]) +
-                                     aParts[11];
-
-        BigInteger d2 = ((BigInteger)aParts[11] << Index[7]) +
-                        ((BigInteger)aParts[9] << Index[6]) +
-                        ((BigInteger)aParts[15] << Index[3]) +
-                        ((BigInteger)aParts[14] << Index[2]) +
-                        ((BigInteger)aParts[13] << Index[1]) +
-                                     aParts[12];
-
-        BigInteger d3 = ((BigInteger)aParts[12] << Index[7]) +
-                        ((BigInteger)aParts[10] << Index[5]) +
-                        ((BigInteger)aParts[9] << Index[4]) +
-                        ((BigInteger)aParts[8] << Index[3]) +
-                        ((BigInteger)aParts[15] << Index[2]) +
-                        ((BigInteger)aParts[14] << Index[1]) +
-                                     aParts[13];
-
-        BigInteger d4 = ((BigInteger)aParts[13] << Index[7]) +
-                        ((BigInteger)aParts[11] << Index[5]) +
-                        ((BigInteger)aParts[10] << Index[4]) +
-                        ((BigInteger)aParts[9] << Index[3]) +
-                        ((BigInteger)aParts[15] << Index[1]) +
-                                     aParts[14];
+        var t = BigInteger.Parse(tString, System.Globalization.NumberStyles.HexNumber);
+        var s1 = BigInteger.Parse(s1String, System.Globalization.NumberStyles.HexNumber);
+        var s2 = BigInteger.Parse(s2String, System.Globalization.NumberStyles.HexNumber);
+        var s3 = BigInteger.Parse(s3String, System.Globalization.NumberStyles.HexNumber);
+        var s4 = BigInteger.Parse(s4String, System.Globalization.NumberStyles.HexNumber);
+        var d1 = BigInteger.Parse(d1String, System.Globalization.NumberStyles.HexNumber);
+        var d2 = BigInteger.Parse(d2String, System.Globalization.NumberStyles.HexNumber);
+        var d3 = BigInteger.Parse(d3String, System.Globalization.NumberStyles.HexNumber);
+        var d4 = BigInteger.Parse(d4String, System.Globalization.NumberStyles.HexNumber);
 
         d1 = P256X2 - d1;
         d2 = P256X2 - d2;
         d3 = P256 - d3;
         d4 = P256 - d4;
 
-        BigInteger r = t + ((s1 + s2) << 1) + s3 + s4 + d1 + d2 + d3 + d4;
+        var r = t + ((s1 + s2) << 1) + s3 + s4 + d1 + d2 + d3 + d4;
+
+        while (r >= P256) r -= P256;
+
+        return r;
+    }
+
+    public static BigInteger FastModuloP256WithBuilder(BigInteger a)
+    {
+        var aParts = new uint[16];
+        for (var i = 0; i < 16; i++)
+        {
+            aParts[i] = (uint)((a >> (i * 32)) & 0xFFFFFFFF);
+        }
+
+        var zeros = "00000000";
+        var hexParts = new string[16];
+        for (var i = 0; i < 16; i++)
+        {
+            hexParts[i] = aParts[i].ToString("X8");
+        }
+
+        var sb = new StringBuilder();
+
+        sb.Append("0");
+        for (int i = 7; i >= 0; i--)
+        {
+            sb.Append(hexParts[i]);
+        }
+        var tString = sb.ToString();
+
+        sb.Clear();
+        for (int i = 15; i >= 11; i--)
+        {
+            sb.Append(hexParts[i]);
+        }
+        sb.Append(zeros);
+        sb.Append(zeros);
+        sb.Append(zeros);
+        var s1String = sb.ToString();
+
+        sb.Clear();
+        sb.Append(zeros);
+        for (int i = 15; i >= 12; i--)
+        {
+            sb.Append(hexParts[i]);
+        }
+        sb.Append(zeros);
+        sb.Append(zeros);
+        sb.Append(zeros);
+        var s2String = sb.ToString();
+
+        sb.Clear();
+        sb.Append(hexParts[15]);
+        sb.Append(hexParts[14]);
+        sb.Append(zeros);
+        sb.Append(zeros);
+        sb.Append(zeros);
+        sb.Append(hexParts[10]);
+        sb.Append(hexParts[9]);
+        sb.Append(hexParts[8]);
+        var s3String = sb.ToString();
+
+        sb.Clear();
+        sb.Append(hexParts[8]);
+        sb.Append(hexParts[13]);
+        sb.Append(hexParts[15]);
+        sb.Append(hexParts[14]);
+        sb.Append(hexParts[13]);
+        sb.Append(hexParts[11]);
+        sb.Append(hexParts[10]);
+        sb.Append(hexParts[9]);
+        var s4String = sb.ToString();
+
+        sb.Clear();
+        sb.Append(hexParts[10]);
+        sb.Append(hexParts[8]);
+        sb.Append(zeros);
+        sb.Append(zeros);
+        sb.Append(zeros);
+        sb.Append(hexParts[13]);
+        sb.Append(hexParts[12]);
+        sb.Append(hexParts[11]);
+        var d1String = sb.ToString();
+
+        sb.Clear();
+        sb.Append("0");
+        sb.Append(hexParts[11]);
+        sb.Append(hexParts[9]);
+        sb.Append(zeros);
+        sb.Append(zeros);
+        sb.Append(hexParts[15]);
+        sb.Append(hexParts[14]);
+        sb.Append(hexParts[13]);
+        sb.Append(hexParts[12]);
+        var d2String = sb.ToString();
+
+        sb.Clear();
+        sb.Append(hexParts[12]);
+        sb.Append(zeros);
+        sb.Append(hexParts[10]);
+        sb.Append(hexParts[9]);
+        sb.Append(hexParts[8]);
+        sb.Append(hexParts[15]);
+        sb.Append(hexParts[14]);
+        sb.Append(hexParts[13]);
+        var d3String = sb.ToString();
+
+        sb.Clear();
+        sb.Append("0");
+        sb.Append(hexParts[13]);
+        sb.Append(zeros);
+        sb.Append(hexParts[11]);
+        sb.Append(hexParts[10]);
+        sb.Append(hexParts[9]);
+        sb.Append(zeros);
+        sb.Append(hexParts[15]);
+        sb.Append(hexParts[14]);
+        var d4String = sb.ToString();
+
+        var t = BigInteger.Parse(tString, System.Globalization.NumberStyles.HexNumber);
+        var s1 = BigInteger.Parse(s1String, System.Globalization.NumberStyles.HexNumber);
+        var s2 = BigInteger.Parse(s2String, System.Globalization.NumberStyles.HexNumber);
+        var s3 = BigInteger.Parse(s3String, System.Globalization.NumberStyles.HexNumber);
+        var s4 = BigInteger.Parse(s4String, System.Globalization.NumberStyles.HexNumber);
+        var d1 = BigInteger.Parse(d1String, System.Globalization.NumberStyles.HexNumber);
+        var d2 = BigInteger.Parse(d2String, System.Globalization.NumberStyles.HexNumber);
+        var d3 = BigInteger.Parse(d3String, System.Globalization.NumberStyles.HexNumber);
+        var d4 = BigInteger.Parse(d4String, System.Globalization.NumberStyles.HexNumber);
+
+        d1 = P256X2 - d1;
+        d2 = P256X2 - d2;
+        d3 = P256 - d3;
+        d4 = P256 - d4;
+
+        var r = t + ((s1 + s2) << 1) + s3 + s4 + d1 + d2 + d3 + d4;
 
         while (r >= P256) r -= P256;
 
--- ELO/ELT/MathUtilitiesTests.cs
+++ ELO/ELT/MathUtilitiesTests.cs
@@ -20,6 +20,19 @@ public class MathUtilitiesTests
     }
 
     [Test]
+    public void FastModuloP256WithBuilderTest()
+    {
+        BigInteger input = BigInteger.Parse("2346391098346120082933372396717514455605377270242392040502406036558391451327823432224103120108044879730919178283232490692426139919419451284040618676301796");
+        var expected = BigInteger.Parse("69187469364232031836548821531971153808731075654725806004116076052366752432012");
+        var expectedStandard = input % Curve.P;
+
+        var result = MathUtilities.FastModuloP256WithBuilder(input);
+
+        Assert.AreEqual(expected, result);
+        Assert.AreEqual(expectedStandard, result);
+    }
+
+    [Test]
     public void NAFTest()
     {
         BigInteger input = 27;
