84bf86183f42c53f5e5159c655cdb192884f5136
V <V@DESKTOP-2KS75VT>
2024-07-24 20:12:12 +0100

    Test for System.Cryptography added

---
+++ ELO/ELT/SystemCryptographyTests.cs
@@ -0,0 +1,28 @@
+﻿using System.Security.Cryptography;
+
+namespace ELT;
+
+[TestFixture]
+public class SystemCryptographyTests
+{
+    [Test]
+    public static void SystemCryptographyTest()
+    {
+        using var alice = new ECDiffieHellmanCng(CngKey.Create(CngAlgorithm.ECDiffieHellmanP256));
+        using var bob = new ECDiffieHellmanCng(CngKey.Create(CngAlgorithm.ECDiffieHellmanP256));
+
+        alice.KeyDerivationFunction = ECDiffieHellmanKeyDerivationFunction.Hash;
+        alice.HashAlgorithm =  CngAlgorithm.Sha256;
+
+        bob.KeyDerivationFunction = ECDiffieHellmanKeyDerivationFunction.Hash;
+        bob.HashAlgorithm = CngAlgorithm.Sha256;
+
+        var alicePublicKey = alice.PublicKey.ToByteArray();
+        var bobPublicKey = bob.PublicKey.ToByteArray();
+
+        var aliceSharedSecret = alice.DeriveKeyMaterial(CngKey.Import(bobPublicKey, CngKeyBlobFormat.EccPublicBlob));
+        var bobSharedSecret = bob.DeriveKeyMaterial(CngKey.Import(alicePublicKey, CngKeyBlobFormat.EccPublicBlob));
+
+        Assert.AreEqual(aliceSharedSecret, bobSharedSecret);
+    }
+}
